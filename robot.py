534c5a72ad0f1ad64f66047994a1df1e9cdc3d52
SamP-S <sam.ps1@outlook.com>
2025-09-12 14:04:01 +0100

    draw DH parameters

--- robot.py
+++ robot.py
@@ -1,77 +1,98 @@
-import pygame
+import pygame as pg
 from pygame.locals import *
 
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+from math import *
+
 WINDOW_WIDTH = 480
 WINDOW_HEIGHT = 360
 
-cubeVertices = ((-1, -1, -1), (-1, -1, 1), (1, -1, 1), (1, -1, -1), (-1, 1, -1), (-1, 1, 1), (1, 1, 1), (1, 1, -1))
-cubeQuads = ((0, 1, 2, 3), (0, 4, 5, 1), (1, 5, 6, 2), (2, 6, 7, 3), (3, 7, 4, 0), (4, 7, 6, 5))
-cubeEdges = ((0, 1), (1, 2), (2, 3), (3, 0), (4, 5), (5, 6), (6, 7), (7, 4), (0, 4), (1, 5), (2, 6), (3, 7))
-cubePoints = (0, 1, 2, 3, 4, 5, 6, 7)
-
-
 # definitions
 def glWindow():
-    pygame.init()
-    window = pygame.display
-    #pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF|OPENGL)
-    pygame.display.set_caption("bruh")
+    pg.init()
+    window = pg.display
+    pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), DOUBLEBUF|OPENGL)
+    pg.display.set_caption("bruh")
     gluPerspective(45, WINDOW_WIDTH/WINDOW_HEIGHT, 0.1, 50)
-    glTranslatef(0, 0, -5)
+    glTranslatef(0, -1, -5)
     glScalef(0.5, 0.5, 0.5)
-
+    glRotatef(-90, 1, 0, 0)
+    glRotatef(-45, 0, 0, 1)
+    glLineWidth(5.0)
+    
 
 def handleEvents():
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
             quit()
 
-
-def quadCube():
-    glBegin(GL_QUADS)
-    for cubeQuad in cubeQuads:
-        for cubeVertex in cubeQuad:
-            glVertex3fv(cubeVertices[cubeVertex])
-    glEnd()
-
-
-def edgeCube():
+# d: link offset    (prismatic)
+# theta: joint angle    (theta)
+# a: link length       
+# alpha: link twist   
+def applyDH(d, theta, a, alpha):
+    # draw variable transforms
+    glColor3fv((1, 1, 0))
     glBegin(GL_LINES)
-    for cubeEdge in cubeEdges:
-        for cubeVertex in cubeEdge:
-            glVertex3fv(cubeVertices[cubeVertex])
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((0, 0, d))
     glEnd()
-
-
-def pointCube():
-    glBegin(GL_POINTS)
-    for cubeVertex in cubePoints:
-        glVertex3fv(cubeVertices[cubeVertex])
+    # apply variable transforms
+    glRotatef(theta, 0, 0, 1)
+    glTranslate(0, 0, d)
+    
+    # draw static transforms
+    glColor3fv((1, 0, 1))
+    glBegin(GL_LINES)
+    glVertex3fv((0, 0, 0))
+    glVertex3fv((a, 0, 0))
     glEnd()
-
-
+    # apply static transforms
+    glTranslate(a, 0, 0)
+    glRotatef(alpha, 1, 0, 0)
+    
+def drawOrigin():
+    axes = [(0, 0, 1), (0, 1, 0), (1, 0, 0)]
+    
+    glBegin(GL_LINES)
+    for axis in axes:
+        glColor3fv(axis)
+        glVertex3fv((0, 0, 0))
+        glVertex3fv(axis)
+    glEnd()
+    
+    
 def main():
-    print("pyopengl")
+    print("pyrobot")
     glWindow()
     frame = 0
     while True:
         handleEvents()
         glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
-        glRotatef(0.5, 1, 1.5, 1)
-        cube_type = (frame // 60) % 3
-        if cube_type == 0:
-            quadCube()
-        elif cube_type == 1:
-            edgeCube()
-        else:
-            pointCube()
-        pygame.display.flip()
-        pygame.time.wait(10)
+        glPushMatrix()
+        
+        # glRotatef(0.5 * frame, 1, 1.5, 1)
+        q1 = 1 + 1 * sin(pg.time.get_ticks() / 1000)
+        q2 = -45 + 90 * sin(pg.time.get_ticks() / 1000)
+        
+        drawOrigin()
+        
+        applyDH(2, 0, 0, q2)
+        
+        drawOrigin()
+        applyDH(2, 90, 2, q2)
+        drawOrigin()
+        
+        # applyDH(2, 0, 0, 0)
+        # drawOrigin()
+        
+        glPopMatrix()
+        pg.display.flip()
+        pg.time.wait(10)
+        
         frame += 1
 
 
