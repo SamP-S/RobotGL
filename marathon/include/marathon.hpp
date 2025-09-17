caa42f90eef94a5fefffe996a6a56ef7054897c6
SamP-S <sam.ps1@outlook.com>
2025-09-17 18:46:54 +0100

    correct backend bitwise enum flag usage

--- marathon/include/marathon.hpp
+++ marathon/include/marathon.hpp
@@ -8,21 +8,30 @@
 
 namespace marathon {
 
-    enum Backends {
-        DEFAULT = 0x00,
-        OPENGL  = 0x01,
-        VULKAN  = 0x01 << 1,
-        SDL2    = 0x01 << 2,
-        GLFW    = 0x01 << 3,
-        NONE    = 0x01 << 4
+
+    enum Backends : int32_t {
+        NONE        = 0x0000,
+        // gpu api
+        OPENGL      = 0x0001,
+        VULKAN      = 0x0002,
+        RENDERER    = 0x000F,
+        // window context
+        SDL2        = 0x0010,
+        GLFW        = 0x0020,
+        WINDOW      = 0x00F0,
+        // gui toolkit
+        IMGUI       = 0x0100,
+        GUI         = 0x0F00,
     };
+    typedef int32_t BackendFlags;
+
 
     extern const std::array<int, 4> validBackends;
 
     /** @brief Initialise all modules
         @return Returns the number of modules that failed to boot
     */
-    int Init(Backends flags = Backends::DEFAULT);
+    int Init(BackendFlags flags = OPENGL | SDL2);
 
     /** @brief Quit all modules
         @return number of modules that failed to quit
