a7ac62aaa41ced99eabad75e3d8da002c6b631bb
SamP-S <sam.ps1@outlook.com>
2025-09-19 18:24:35 +0100

    event overload<< for cout

--- marathon/include/events/events.hpp
+++ marathon/include/events/events.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 // std lib
+#include <ostream>
 #include <variant>
 
 // internal
@@ -10,7 +11,18 @@ namespace marathon {
 
 namespace events {
 
+enum EventType {
+    EVENT_COMMON,
+    EVENT_KEY,
+    EVENT_MOUSE,
+    EVENT_QUIT,
+    EVENT_WINDOW,
+};
+
 typedef std::variant<int, float, double, bool, std::string> EventProperty;
+
+std::ostream& operator<<(std::ostream& os, const EventProperty& ep);
+
 
 /// @brief Represents an event with a name and associated data.
 /// @details An event consists of constant data, so it cannot be modified.
@@ -22,6 +34,8 @@ struct Event {
     Event() = default;
     Event(const std::string& name, const std::unordered_map<std::string, EventProperty>& data);
 };
+
+std::ostream& operator<<(std::ostream& os, const Event& event);
 
 // events API facade
 bool Init(BackendFlags flags);
--- marathon/src/events/events.cpp
+++ marathon/src/events/events.cpp
@@ -13,8 +13,27 @@ namespace {
     static IEventSystem* instance = nullptr;
 }
 
+std::ostream& operator<<(std::ostream& os, const EventProperty& ep) {
+    std::visit([&os](const auto& value) {
+        if constexpr (std::is_same_v<decltype(value), bool>) {
+            os << (value ? "true" : "false");
+        } else {
+            os << value;
+        }
+    }, ep);
+    return os;
+}
+
 Event::Event(const std::string& name, const std::unordered_map<std::string, EventProperty>& data)
     : name(name), data(data) {}
+
+std::ostream& operator<<(std::ostream& os, const Event& event) {
+    os << "Event Name: " << event.name << std::endl;
+    for (const auto& [key, value] : event.data) {
+        os << "\tKey: " << key << ", Value: " << value << std::endl;
+    }
+    return os;
+}
 
 // events API facade
 bool Init(BackendFlags flags) {
