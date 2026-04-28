6c15785ca0ccf9095484fc33ac49ae4fce8d3792
SamP-S <sam.ps1@outlook.com>
2026-04-28 21:05:03 +0100

    rm unfinished C++; rm git submodules

--- compile.sh
+++
@@ -1,5 +0,0 @@
-if [[ "$1" == "clean" ]]; then
-    rm -rf build
-fi
-cmake -S . -B build
-cmake --build build
--- marathon/src/core/resource.cpp
+++
@@ -1,8 +0,0 @@
-#include "core/resource.hpp"
-
-namespace marathon {
-
-Resource::Resource(const std::string& type)
-    : Object(type) {}
-
-} // marathon
